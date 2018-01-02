40dd21efdf52534159280d923c3d5282875a592d
Sam Elkind <samuel.elkind@gmail.com>
2018-01-02 10:55:15 -0700

    implement shell sort
    added shell sort as the third candidate for fastest letter frequency
    distribution builder.

---
+++ src/Shell.py
@@ -0,0 +1,18 @@
+from sorting.src.sorter import Sorter
+
+
+class Shell(Sorter):
+
+    def sort(self, values: list):
+        N = len(values)
+        h = 1
+
+        while h < N/3:
+            h = 3 * h + 1
+        while h >= 1:
+            for i in range(h, N):
+                j = i
+                while j >= h and a[j] < a[j - h]:
+                    self.exchange(values, j, j - h)
+                    j -= h
+            h = h / 3
