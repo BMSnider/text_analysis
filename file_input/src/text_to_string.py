b994a1fd000588277a7ae71b7b744ef170a15560
Sam Elkind <samuel.elkind@gmail.com>
2018-04-18 22:03:54 -0600

    wrapped file processes in try: except: block

--- file_input/src/text_to_string.py
+++ file_input/src/text_to_string.py
@@ -2,13 +2,15 @@ class TextToString:
     """Converts .txt file to raw string."""
     
     def __init__(self, path):
-        """Takes in file and creates formatted namedtuple"""
         self.path = path
 
     def convert_txt(self):
         str_file = ''
-        with open(self.path, "r") as txt_file:       
-            for line in txt_file:
-                str_file += str(line)        
+        try:
+            with open(self.path, "r") as txt_file:
+                for line in txt_file:
+                    str_file += str(line)
+        except IOError:
+            print("{} could not be converted to a string".format(self.path))
     
         return str_file
