14bff372b327e3a2f2c1c0eb0970726738b9eda2
Sam Elkind <samuel.elkind@gmail.com>
2018-04-21 20:34:03 -0600

    completed basic PathManager unittests

--- file_input/tests/test_path_manager.py
+++ file_input/tests/test_path_manager.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import collections
 from text_analysis.file_input.src.path_manager import PathManager
 from text_analysis.file_input.src.text_to_string import TextToString
 
@@ -8,6 +9,11 @@ class TestPathManager(unittest.TestCase):
     def setUp(self):
         self.test_dir_path = os.getcwd()
         self.test_file_path = self.test_dir_path + "/text_to_string_test_data.txt"
+        self.test_file_path_data = """`1234567890 -=qwertyuiop[]\\
+asdfghjkl;'zxcvbnm,./~!@#$%
+
+^&*()_+QWERTYUIOP{}|ASDFGHJK
+    L:"ZXCVBNM<>?""".encode("unicode-escape")
 
     def test_get_files_dir_input(self):
         test = PathManager(self.test_dir_path)
@@ -36,3 +42,15 @@ class TestPathManager(unittest.TestCase):
         test = PathManager(self.test_file_path)
         converter = test.get_file_converter("fish.fsx")
         self.assertIsNone(converter)
+
+    def test_format_output(self):
+        test = PathManager(self.test_file_path)
+        converter = test.get_file_converter(self.test_file_path)
+        data = converter.convert_txt()
+        output = test.format_output(self.test_file_path, data)
+
+        expected_output_format = collections.namedtuple('converted_string', 'path file raw_string')
+        expected_file_name = os.path.basename(self.test_file_path)
+        expected_output = expected_output_format(path=self.test_file_path, file=expected_file_name,
+                                                 raw_string=self.test_file_path_data)
+        self.assertEqual(expected_output, output)
